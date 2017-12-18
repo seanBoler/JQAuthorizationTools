4ff2e778d929e9a0ea34ab0da4514013751500a1
zhangjiaqi <zhangjiaqi@zhangjiaqi.local>
2017-12-18 14:59:35 +0800

    Initial Commit

---
+++ JQAuthorizationTool/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  JQAuthorizationTool
+//
+//  Created by zhangjiaqi on 2017/12/18.
+//  Copyright © 2017年 zhangjiaqi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
