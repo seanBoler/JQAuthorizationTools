d10fe72d7b51afa0e919c390f02062995a067295
zhangjiaqi <zhangjiaqi@zhangjiaqi.local>
2017-12-19 16:24:50 +0800

    first commit

---
+++ JQAuthorizationTool/AuthorizationTool/JQAuthorizationTools.h
@@ -0,0 +1,13 @@
+//
+//  JQAuthorizationTools.h
+//  JQAuthorizationTool
+//
+//  Created by zhangjiaqi on 2017/12/18.
+//  Copyright © 2017年 zhangjiaqi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JQAuthorizationTools : NSObject
+
+@end
---
+++ JQAuthorizationTool/AuthorizationTool/JQAuthorizationTools.m
@@ -0,0 +1,13 @@
+//
+//  JQAuthorizationTools.m
+//  JQAuthorizationTool
+//
+//  Created by zhangjiaqi on 2017/12/18.
+//  Copyright © 2017年 zhangjiaqi. All rights reserved.
+//
+
+#import "JQAuthorizationTools.h"
+
+@implementation JQAuthorizationTools
+
+@end
